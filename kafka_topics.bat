278031afc63aa4c525c765a0294211cc1f917696
maheshshindekj <36610991+maheshshindekj@users.noreply.github.com>
2019-02-21 16:47:19 -0800

    Add files via upload

---
+++ kafka_topics.bat
@@ -0,0 +1,20 @@
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  response_topic
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  user_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  multiplex_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  movie_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  multiplexadmin_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  review_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  showtiming_request
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  completePayment
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic  fetchBillingDetails
+
+
+C:\kafka_2.11-1.0.1\bin\windows\kafka-topics.bat --list --zookeeper localhost:2181
